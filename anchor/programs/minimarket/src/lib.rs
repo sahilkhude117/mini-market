53e757f697b2575f7cd4fb9b4218674f14d82d28
sahil <sahilkhude11@gmail.com>
2025-11-23 09:13:58 +0000

    program: init

--- anchor/programs/minimarket/src/lib.rs
+++ anchor/programs/minimarket/src/lib.rs
@@ -1,6 +1,21 @@
 #![allow(clippy::result_large_err)]
 
 use anchor_lang::prelude::*;
+pub mod constants;
+pub mod errors;
+pub mod events;
+pub mod instructions;
+pub mod states;
+pub mod utils;
+
+use instructions::{
+    betting::*, create_market::*, deposite_liquidity::*, get_oracle_res::*, init::*, token_mint::*,
+    withdraw::*,
+};
+use states::{
+    global::GlobalParams,
+    market::{BettingParams, MarketParams},
+};
 
 declare_id!("Count3AcZucFDPSFBAeHkQ6AvttieKUkyJ8HiQGhQwe");
 
@@ -8,63 +23,7 @@ declare_id!("Count3AcZucFDPSFBAeHkQ6AvttieKUkyJ8HiQGhQwe");
 pub mod minimarket {
     use super::*;
 
-    pub fn close(_ctx: Context<CloseMinimarket>) -> Result<()> {
-        Ok(())
+    pub fn initialize(ctx: Context<Initialize>, params: GlobalParams) -> Result<()> {
+        init(ctx, params)
     }
-
-    pub fn decrement(ctx: Context<Update>) -> Result<()> {
-        ctx.accounts.minimarket.count = ctx.accounts.minimarket.count.checked_sub(1).unwrap();
-        Ok(())
-    }
-
-    pub fn increment(ctx: Context<Update>) -> Result<()> {
-        ctx.accounts.minimarket.count = ctx.accounts.minimarket.count.checked_add(1).unwrap();
-        Ok(())
-    }
-
-    pub fn initialize(_ctx: Context<InitializeMinimarket>) -> Result<()> {
-        Ok(())
-    }
-
-    pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
-        ctx.accounts.minimarket.count = value.clone();
-        Ok(())
-    }
-}
-
-#[derive(Accounts)]
-pub struct InitializeMinimarket<'info> {
-    #[account(mut)]
-    pub payer: Signer<'info>,
-
-    #[account(
-  init,
-  space = 8 + Minimarket::INIT_SPACE,
-  payer = payer
-    )]
-    pub minimarket: Account<'info, Minimarket>,
-    pub system_program: Program<'info, System>,
-}
-#[derive(Accounts)]
-pub struct CloseMinimarket<'info> {
-    #[account(mut)]
-    pub payer: Signer<'info>,
-
-    #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
-    )]
-    pub minimarket: Account<'info, Minimarket>,
-}
-
-#[derive(Accounts)]
-pub struct Update<'info> {
-    #[account(mut)]
-    pub minimarket: Account<'info, Minimarket>,
-}
-
-#[account]
-#[derive(InitSpace)]
-pub struct Minimarket {
-    count: u8,
 }
